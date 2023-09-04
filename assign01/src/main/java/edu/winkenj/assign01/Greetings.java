db30e47ad6f515e64581896e46e7c91139d9a7a2
Jared Winkens <winkenj@sunypoly.edu>
2023-09-04 09:09:05 -0400

    Completed Assignment 1.

---
+++ assign01/src/main/java/edu/winkenj/assign01/Greetings.java
@@ -0,0 +1,7 @@
+package edu.winkenj.assign01;
+
+public class Greetings {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
--- assign01/src/test/java/edu/winkenj/assign01/Test_Greetings.java
+++ assign01/src/test/java/edu/winkenj/assign01/Test_Greetings.java
@@ -1,4 +1,4 @@
-package edu.realemj.assign01;
+package edu.winkenj.assign01;
 // NOTE: CHANGE realemj to YOUR SITNETID!!!
 import edu.realemj.testing.*;
 import org.testng.Assert;

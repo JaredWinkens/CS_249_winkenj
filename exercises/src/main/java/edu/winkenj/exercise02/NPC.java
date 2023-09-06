86df92207dc94adae28a64cb9edea29d78839f9f
Jared Winkens <winkenj@sunypoly.edu>
2023-09-06 13:49:27 -0400

    Added NPC class.

---
+++ exercises/src/main/java/edu/winkenj/exercise02/NPC.java
@@ -0,0 +1,16 @@
+package edu.winkenj.exercise02;
+
+public class NPC {
+    private int health = 100;
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void setHealth(int h) {
+        health = h;
+        health = Math.max(health,0);
+        health = Math.min(health,100);
+
+    }
+}
--- exercises/src/main/java/edu/winkenj/exercise02/Wasteland.java
+++ exercises/src/main/java/edu/winkenj/exercise02/Wasteland.java
@@ -3,5 +3,12 @@ package edu.winkenj.exercise02;
 public class Wasteland {
     public static void main(String [] args){
         System.out.println("Wasteland");
+
+        NPC jackson = new NPC();
+        NPC forest = new NPC();
+
+        int h = jackson.getHealth();
+        h -= 5;
+        jackson.setHealth(h);
     }
 }
